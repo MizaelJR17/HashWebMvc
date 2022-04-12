c284378c937c5cfb13c3e8993f23b02d7b1433ab
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br>
2022-04-12 19:18:38 -0300

    CRUD Scanffolding

---
+++ HashWebMvc/Data/HashWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HashWebMvc.Models.ViewModels;
+
+namespace HashWebMvc.Models
+{
+    public class HashWebMvcContext : DbContext
+    {
+        public HashWebMvcContext (DbContextOptions<HashWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HashWebMvc.Models.ViewModels.Department> Department { get; set; }
+    }
+}
--- HashWebMvc/Startup.cs
+++ HashWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using HashWebMvc.Models;
 
 namespace HashWebMvc
 {
@@ -33,6 +35,9 @@ namespace HashWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<HashWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("HashWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
