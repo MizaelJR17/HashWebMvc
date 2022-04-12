cddda1246674983ac4dc8dcc320ff19d2efe7dc9
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br>
2022-04-12 19:08:34 -0300

    Criação da Classe Departmet.cs

---
+++ HashWebMvc/Models/ViewModels/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HashWebMvc.Models;
+namespace HashWebMvc.Models.ViewModels
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
