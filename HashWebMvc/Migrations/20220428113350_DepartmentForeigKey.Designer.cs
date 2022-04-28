793db6be154f5539e1abbe9f3157b77db10af02d
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br>
2022-04-28 08:43:10 -0300

    Foreingn key not null

--- HashWebMvc/Migrations/20220428113350_DepartmentForeigKey.Designer.cs
+++ HashWebMvc/Migrations/20220428113350_DepartmentForeigKey.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using HashWebMvc.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HashWebMvc.Migrations
 {
     [DbContext(typeof(HashWebMvcContext))]
-    partial class HashWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20220428113350_DepartmentForeigKey")]
+    partial class DepartmentForeigKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- HashWebMvc/Services/SellerService.cs
+++ HashWebMvc/Services/SellerService.cs
@@ -23,6 +23,8 @@ namespace HashWebMvc.Services
 
         public void Insert(Seller obj )
         {
+
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
